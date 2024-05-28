931e49e2d16c3437f6640ade671b7b721bb73f79
HaotongZhong <haotzhong@student.unimelb.edu.au>
2024-05-28 16:39:10 +0800

    adding searching suggestions feature

--- javascript.js
+++ javascript.js
@@ -1,1 +1,20 @@
-<script> src = "index.html" </script>
+function expandList(){
+    document.getElementById("suggestions-list").classList.remove("fold");
+};
+
+function foldList(){
+    setTimeout(function(){
+        document.getElementById("suggestions-list").classList.add("fold");
+    },50);
+};
+
+
+function getKeyword(keyword){
+    document.querySelector(".search-input").value = keyword;
+};
+
+function redirect(key){
+    if (key.keyCode === 13){
+       location.href = 'pages/productlist.html';
+    };
+};
