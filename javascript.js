19c91e5ce371a15d020d2f0291d4c1492a232594
Ryan Hu <ryhu@student.unimelb.edu.au>
2024-05-28 23:24:50 +1000

    open/close cart

--- javascript.js
+++ javascript.js
@@ -1,33 +1,45 @@
-function expandList(){
+function expandList() {
     document.getElementById("suggestions-list").classList.remove("fold");
-};
+}
 
-function foldList(){
-    setTimeout(function(){
+function foldList() {
+    setTimeout(function() {
         document.getElementById("suggestions-list").classList.add("fold");
-    },50);
-};
+    }, 50);
+}
 
-
-function getKeyword(keyword){
+function getKeyword(keyword) {
     document.querySelector(".search-input").value = keyword;
-};
+}
 
-function redirect(key){
-    if (key.keyCode === 13){
-       location.href = 'pages/productlist.html';
-    };
-};
+function redirect(key) {
+    if (key.keyCode === 13) {
+        location.href = 'pages/productlist.html';
+    }
+}
 
-// Shopping Cart
 
-const openBtn = document.getElementById('open-cart');
-const cart = document.getElementById('shopping-cart');
+//open cart and close cart
 
-openBtn.addEventListener('click', openCart);
+document.addEventListener('DOMContentLoaded', (event) => {
+    const openBtn = document.getElementById('open-cart');
 
-    //openCart
+    openBtn.addEventListener('click', openCart);
+});
+
+document.addEventListener('DOMContentLoaded', (event) => {
+    const openBtn = document.getElementById('close-cart');
+
+    openBtn.addEventListener('click', closeCart);
+});
+
 
 function openCart() {
-    cart.classList.add('open')
-};
+    const cart = document.getElementById('shopping-cart');
+    cart.classList.add('open');
+}
+
+function closeCart() {
+    const cart = document.getElementById('shopping-cart');
+    cart.classList.remove('open');
+}
